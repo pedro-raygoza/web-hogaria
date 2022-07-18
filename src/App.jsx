52abfa61f202e84fcd95061754d242c3fdd27568
Isaí Arellano <isai.arellano@outlook.com>
2022-07-18 16:08:19 -0500

    Estructura de proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+    return (
+        <h1>Componente Nav</h1>
+    )
+}
+
+export default Nav
