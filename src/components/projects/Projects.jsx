22a777585840815e9b4a383612e2eb679abd4db5
jairo <valdezjairo8@gmail.com>
2022-07-21 09:14:37 -0500

    Se realizaron cambion en la seccion de proyectos

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './projects.css'
+import proyec from '../../assets/imara-verde.png'
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Proyectos</h1>
+    <div>
+        <div className="container">
+            <h1 className="titulo">Proyectos</h1>
+            <div className="card">
+            <img className="imara-logo" src={proyec} alt="Imara"/>
+                <h4>Casa Habitacion</h4>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <a href="#">Leer mas</a>
+            </div>
         </div>
+    </div> 
     )
 }
 
