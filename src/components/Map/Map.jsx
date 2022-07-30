580a7badc2eb65a2df6e9fd8fe15feb3b48f01b5
Isaí Arellano <isai.arellano@outlook.com>
2022-07-29 19:06:35 -0500

    Se arreglo problema con iframe para deploy

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ const Map = () => {
         <div>
          <h1>Contacto</h1>
         <div className="container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3712.1093929933177!2d-104.892566!3d21.5034347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842736fd50c854c9%3A0xe88f2342a24ad5f5!2sAv%20Insurgentes%20127a%2C%20Centro%2C%2063000%20Tepic%2C%20Nay.!5e0!3m2!1ses-419!2smx!4v1659130755907!5m2!1ses-419!2smx" 
+        <iframe name="iframe-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3712.1093929933177!2d-104.892566!3d21.5034347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842736fd50c854c9%3A0xe88f2342a24ad5f5!2sAv%20Insurgentes%20127a%2C%20Centro%2C%2063000%20Tepic%2C%20Nay.!5e0!3m2!1ses-419!2smx!4v1659130755907!5m2!1ses-419!2smx" 
         width="600" height="300" style={{border:0}} loading="lazy"></iframe>
         </div>
         </div>
